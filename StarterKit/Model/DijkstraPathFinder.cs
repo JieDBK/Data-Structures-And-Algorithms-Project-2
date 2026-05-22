7e4f860de050637b534a8b03699b741bfebc197d
HiddevBruggen <hiddevanbruggen@gmail.com>
2026-05-22 19:13:17 +0200

    dijkstra

--- StarterKit/Model/DijkstraPathFinder.cs
+++ StarterKit/Model/DijkstraPathFinder.cs
@@ -22,7 +22,56 @@ namespace Model
             List<int[]> ToVisitPositions = [];
             ToVisitPositions.Add(pos);
 
+            while(ToVisitPositions.Count > 0)
+            {
+                int[] currentPos = { -1, -1};
+                int smallestDistance = int.MaxValue;
+
+                foreach(int[] position in ToVisitPositions)
+                {
+                    if(distance[position[0], position[1]] < smallestDistance)
+                    {
+                        currentPos = position;
+                        smallestDistance = distance[position[0], position[1]]; 
+                    }
+                }
+                ToVisitPositions.Remove(currentPos);
+
+                if(currentPos == maze.End) break;
+
+                foreach(int[] move in maze.moves)
+                {
+                    int newRow = currentPos[0] + move[0];
+                    int newCol = currentPos[1] + move[1];
+
+                    int[] newPos = { newRow, newCol };
+                    if(!maze.IsValidMove(newRow, newCol)) continue;
+
+                    int newDistance = distance[currentPos[0], currentPos[1]] + 1;
+
+                    if(newDistance < distance[newRow, newCol])
+                    {
+                        distance[newRow, newCol] = newDistance;
+                        Parents[(newRow, newCol)] = (currentPos[0], currentPos[1]); //child/neighbour is de key, parent is de value, want je werkt van eind naar begin  
+                        ToVisitPositions.Add(newPos);
+                    }
+                }
+            }
+
+            List<int[]> route = [];
+            int[] current = maze.End;
+            while(Parents.ContainsKey((current[0], current[1]))) //de startlocatie is een parent en geen child en heeft dus geen key en dan stopt de loop
+            {
+                route.Add(current);
+                (int row, int col) parent = Parents[(current[0], current[1])];
+                current = new int[] { parent.row, parent.col };
+            }
             
+            route.Reverse();
+            foreach(int[] place in route)
+            {
+                visitedPositions.Enqueue(place);
+            }
         }
    }
 }
